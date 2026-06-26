6a74552a159db41ba52e5ba4cbdaecf079c1e432
unknown <weslleysferreira14@gmail.com>
2026-06-25 22:31:49 -0300

    alterando rota de contato para footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import { Layout } from "./components/Layout.jsx";
 import { Home } from "./pages/Home";
 import { ResultadosPesquisa } from "./pages/ResultadosPesquisa";
 import { CadastroUsuario } from "./pages/CadastroUsuario";
 import { Login } from "./pages/Login";
+import { EditProfile } from "./pages/EditProfile";
+
 import { About } from "./pages/About";
 import { Perfil } from "./pages/Perfil";
 import { PerfilEnderecos } from "./pages/PerfilEnderecos";
@@ -13,34 +16,45 @@ import { PerfilQualidade } from "./pages/PerfilQualidade";
 import { PerfilMidia } from "./pages/PerfilMidia";
 import { PerfilMeusImoveis } from "./pages/PerfilMeusImoveis";
 import { PerfilMeusAnuncios } from "./pages/PerfilMeusAnuncios";
-import { RecuperarSenha } from "./pages/RecuperarSenha";
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<CadastroUsuario />} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
-        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
 
-          <Route index element={<Home />} />
-          {/* Futuramente, novas páginas serão adicionadas aqui */}
-          <Route path="resultados" element={<ResultadosPesquisa />} />
-          <Route path="contact" element={<div className="p-20 text-center text-xl">Contato (Em breve)</div>} />
-          <Route path="about" element={<About />} />
-          <Route path="perfil" element={<Perfil />} />
-          <Route path="perfil/enderecos" element={<PerfilEnderecos />} />
-          <Route path="perfil/seguranca" element={<PerfilSeguranca />} />
-          <Route path="perfil/privacidade" element={<PerfilPrivacidade />} />
-          <Route path="perfil/qualidade" element={<PerfilQualidade />} />
-          <Route path="perfil/midia" element={<PerfilMidia />} />
-          <Route path="perfil/meus-imoveis" element={<PerfilMeusImoveis />} />
-          <Route path="perfil/meus-anuncios" element={<PerfilMeusAnuncios />} />
-        </Route>
-      </Routes>
-    </Router>
+            <Route path="contact" element={<Navigate to="#footer" replace />} />
+
+            <Route
+              path="resultados"
+              element={<ResultadosPesquisa />}
+            />
+
+            <Route path="about" element={<About />} />
+            <Route path="perfil" element={<Perfil />} />
+            <Route path="perfil/editar" element={<EditProfile />} />
+            <Route path="perfil/enderecos" element={<PerfilEnderecos />} />
+            <Route path="perfil/seguranca" element={<PerfilSeguranca />} />
+            <Route path="perfil/privacidade" element={<PerfilPrivacidade />} />
+            <Route path="perfil/qualidade" element={<PerfilQualidade />} />
+            <Route path="perfil/midia" element={<PerfilMidia />} />
+            <Route
+              path="perfil/meus-imoveis"
+              element={<PerfilMeusImoveis />}
+            />
+            <Route
+              path="perfil/meus-anuncios"
+              element={<PerfilMeusAnuncios />}
+            />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
